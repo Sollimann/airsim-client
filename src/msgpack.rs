b501bda476d4dc2a4a77cfeec5467633f70c97a1
Sollimann <kristoffer.solberg@cognite.com>
2022-08-20 15:13:49 +0200

    update client

--- src/msgpack.rs
+++ src/msgpack.rs
@@ -1,15 +1,16 @@
-use async_std::channel::{bounded, Receiver, RecvError, Sender};
+use async_std::channel::{bounded, Receiver, Sender};
 use async_std::io::prelude::*;
 use async_std::net::{TcpStream, ToSocketAddrs};
 use async_std::sync::{Arc, Mutex};
 use async_std::task;
 use futures::future::FutureExt;
-use futures::{pin_mut, select};
+use futures::select;
 use rmp_rpc::message::{Message, Notification, Request, Response};
 use std::collections::HashMap;
 use std::io::Cursor;
 
 use crate::error::NetworkResult;
+use crate::NetworkError;
 
 pub struct MsgPackClient {
     request_sender: Sender<Request>,
@@ -19,7 +20,7 @@ pub struct MsgPackClient {
     response_channels: Arc<Mutex<HashMap<u32, Sender<Response>>>>,
 }
 
-enum ToDo {
+enum Rpc {
     Send(Message),
     Receive(usize),
 }
@@ -42,52 +43,65 @@ impl MsgPackClient {
                 let to_process = select! {
                     maybe_request = request_receiver.recv().fuse() => {
                         if let Ok(request) = maybe_request {
-                            Some(ToDo::Send(Message::Request(request)))
+                            Some(Rpc::Send(Message::Request(request)))
                         } else {
                             None
                         }
                     },
                     maybe_notification = notification_receiver.recv().fuse() => {
                         if let Ok(notification) = maybe_notification {
-                            Some(ToDo::Send(Message::Notification(notification)))
+                            Some(Rpc::Send(Message::Notification(notification)))
                         } else {
                             None
                         }
                     },
                     maybe_bytes_read = stream.read(&mut buf).fuse() => {
                         if let Ok(bytes_read) = maybe_bytes_read {
-                            Some(ToDo::Receive(bytes_read))
+                            Some(Rpc::Receive(bytes_read))
                         } else {
                             None
                         }
                     }
                 };
                 match to_process {
-                    Some(ToDo::Send(m)) => {
+                    Some(Rpc::Send(m)) => {
                         let message = m.pack().expect("Couldn't serialize message");
-                        stream.write_all(&message).await.expect("couldn't send message");
+                        stream.write_all(&message).await.expect("Couldn't send message");
                     }
-                    Some(ToDo::Receive(n)) => {
+                    Some(Rpc::Receive(n)) => {
                         current_message.extend(&buf[..n]);
                         let mut frame = Cursor::new(current_message.clone());
-                        match Message::decode(&mut frame) {
-                            Ok(Message::Notification(n)) => {
-                                inner_notification_sender.send(n).await;
-                            }
-                            Ok(Message::Request(r)) => {
-                                inner_request_sender.send(r).await;
-                            }
+                        let recv_res = match Message::decode(&mut frame) {
+                            Ok(Message::Notification(n)) => inner_notification_sender
+                                .send(n)
+                                .await
+                                .map_err(|e| NetworkError::Send { message: e.to_string() }),
+                            Ok(Message::Request(r)) => inner_request_sender
+                                .send(r)
+                                .await
+                                .map_err(|e| NetworkError::Send { message: e.to_string() }),
                             Ok(Message::Response(r)) => {
                                 let mut senders = res_channels.lock().await;
                                 let sender: Sender<Response> =
                                     senders.remove(&r.id).expect("Got response but no request awaiting it");
-                                sender.send(r).await;
+
+                                // send response to the `request` function
+                                sender
+                                    .send(r)
+                                    .await
+                                    .map_err(|e| NetworkError::Send { message: e.to_string() })
                             }
                             Err(e) => {
-                                // TODO: let's figure something out!
+                                // DecodeError
                                 panic!("{}", e);
                             }
                         };
+
+                        // if error, return it
+                        if let Err(e) = recv_res {
+                            return e;
+                        }
+
                         #[allow(clippy::cast_possible_truncation)]
                         {
                             let (_, remaining) = current_message.split_at(frame.position() as usize);
@@ -107,165 +121,30 @@ impl MsgPackClient {
         })
     }
 
-    pub async fn request(&self, request: Request) -> Result<Response, RecvError> {
+    pub async fn request(&self, request: Request) -> Result<Response, NetworkError> {
         let (response_sender, response_receiver) = bounded(1);
 
-        // TODO: check if there was something
+        // add the response sender (forwards the response from the server) by request id
         let _ = self.response_channels.lock().await.insert(request.id, response_sender);
 
-        self.request_sender.send(request).await;
-        response_receiver.recv().await
+        // forward request to the thread that then forwards it to the MessagePack-RPC server
+        // the response is added to the response channel
+        let send_res = self.request_sender.send(request).await;
+        if send_res.is_err() {
+            let e = format!("Failed to send request: {:?}", send_res);
+            return Err(NetworkError::Send { message: e });
+        }
+
+        // return result from request which is forwarded from the background thread above
+        response_receiver.recv().await.map_err(|e| NetworkError::Recv(e))
     }
 
-    pub async fn notify(&self, notification: Notification) -> Result<(), RecvError> {
-        self.notification_sender.send(notification).await;
+    pub async fn _notify(&self, notification: Notification) -> Result<(), NetworkError> {
+        let res = self.notification_sender.send(notification.to_owned()).await;
+        if res.is_err() {
+            let e = format!("Failed to send notification: {:?}", notification);
+            return Err(NetworkError::Send { message: e });
+        }
         Ok(())
     }
 }
-
-// msgpack MsgPackClient used to interface with the airsim
-// msgpack server
-// pub struct MsgPackMsgPackClient {
-//     request_sender: Sender<Request>,
-//     //request_receiver: Receiver<Request>,
-//     notification_sender: Sender<Notification>,
-//     //notificaiton_receiver: Receiver<Notification>,
-//     response_channels: Arc<Mutex<HashMap<u32, Sender<Response>>>>,
-// }
-
-// /// todo
-// enum Rpc {
-//     Send(Message),
-//     Receive(usize),
-// }
-
-// impl MsgPackMsgPackClient {
-//     /// Establish a TCP socket connection to the `MessagePack-RPC` server
-//     /// running in a background thread
-//     pub async fn connect(addrs: impl ToSocketAddrs) -> NetworkResult<Self> {
-//         // Opens a TCP connection to a remote host
-//         let mut stream = TcpStream::connect(addrs).await.unwrap();
-
-//         // all response channels
-//         let response_channels = Arc::new(Mutex::new(HashMap::new()));
-
-//         // create multiple bounded channels with only 1 size message queue
-//         let (request_sender, request_receiver) = bounded::<Request>(1);
-//         let (inner_request_sender, _inner_request_receiver) = bounded::<Request>(1);
-//         let (notification_sender, notification_receiver) = bounded::<Notification>(1);
-//         let (inner_notification_sender, _inner_notification_receiver) = bounded::<Notification>(1);
-//         let res_channels = Arc::clone(&response_channels);
-
-//         task::spawn(async move {
-//             let mut current_message: Vec<u8> = vec![];
-
-//             // pre-allocate a vector buffer that can fit 1024 bytes
-//             let mut buf = vec![0_u8; 1024];
-
-//             let request_to_server = request_receiver.recv().fuse();
-//             let notification_to_server = notification_receiver.recv().fuse();
-//             // let stream_from_server = stream.read(&mut buf).fuse();
-
-//             pin_mut!(request_to_server, notification_to_server);
-
-//             // start loop that listens to incomning messages on three channels
-//             // using the select! future. The `to_process` variable will be set
-//             // when either of the three callbacks are invoked
-//             loop {
-//                 let to_process = select! {
-//                     next = request_to_server => {
-//                         if let Ok(request) = next {
-//                             Some(Rpc::Send(Message::Request(request)))
-//                         } else {
-//                             None
-//                         }
-//                     },
-//                     next = notification_to_server => {
-//                         if let Ok(notification) = next {
-//                             Some(Rpc::Send(Message::Notification(notification)))
-//                         } else {
-//                             None
-//                         }
-//                     },
-//                     next = stream.read(&mut buf).fuse() => {
-//                         if let Ok(bytes_read) = next {
-//                             Some(Rpc::Receive(bytes_read))
-//                         } else {
-//                             None
-//                         }
-//                     },
-//                 };
-
-//                 match to_process {
-//                     // handler for outbound messages
-//                     Some(Rpc::Send(msg)) => {
-//                         // serialize message to bytes using msgpack
-//                         let message = msg.pack().expect("Couldn't serialize message");
-
-//                         // writes entire buffer to byte stream
-//                         stream.write_all(&message).await.expect("couldn't send message");
-//                     }
-//                     // Handler for inbound
-//                     Some(Rpc::Receive(n)) => {
-//                         //
-//                         current_message.extend(&buf[..n]);
-//                         let mut frame = Cursor::new(current_message.clone());
-//                         match Message::decode(&mut frame) {
-//                             Ok(Message::Notification(n)) => {
-//                                 let _ = inner_notification_sender.send(n).await;
-//                             }
-//                             Ok(Message::Request(r)) => {
-//                                 let _ = inner_request_sender.send(r).await;
-//                             }
-//                             Ok(Message::Response(r)) => {
-//                                 let mut senders = res_channels.lock().await;
-//                                 let sender: Sender<Response> =
-//                                     senders.remove(&r.id).expect("Got response but no request awaiting it");
-
-//                                 // send response to the `request` function
-//                                 let _ = sender.send(r).await;
-//                             }
-//                             Err(decode_err) => {
-//                                 // TODO: let's figure something out!
-//                                 panic!("{}", decode_err);
-//                             }
-//                         };
-//                         #[allow(clippy::cast_possible_truncation)]
-//                         {
-//                             let (_, remaining) = current_message.split_at(frame.position() as usize);
-//                             current_message = remaining.to_vec();
-//                         }
-//                     }
-//                     None => {}
-//                 }
-//             }
-//         });
-
-//         Ok(Self {
-//             request_sender,
-//             notification_sender,
-//             response_channels,
-//         })
-//     }
-
-//     /// todo
-//     pub async fn request(&self, request: Request) -> Result<Response, RecvError> {
-//         let (response_sender, response_receiver) = bounded(1);
-
-//         // add the response sender (forwards the response from the server) by request id
-//         let _ = self.response_channels.lock().await.insert(request.id, response_sender);
-
-//         // forward request to the thread that then forwards it to the MessagePack-RPC server
-//         // the response is added to the response channel
-//         let _ = self.request_sender.send(request).await;
-
-//         // return result from request which is forwarded from the background thread above
-//         response_receiver.recv().await
-//     }
-
-//     /// todo
-//     pub async fn notify(&self, notification: Notification) -> Result<(), RecvError> {
-//         let _ = self.notification_sender.send(notification).await;
-//         Ok(())
-//     }
-// }
